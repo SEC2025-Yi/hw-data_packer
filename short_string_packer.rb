ee8d015f78dcff17358ea26500784010c92e7109
Yi Kuo <kuokuoyiyi@gmail.com>
2025-03-04 17:42:34 +0800

    feat: implement packer

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -7,7 +7,8 @@ module ShortStringPacker
   #   str - String object
   # Returns: a Integer object
   def self.pack(str)
-    # IMPLEMENT THIS METHOD
+    numbers = str.chars.map { |chr| chr.ord - 'a'.ord + 1 }
+    numbers.reduce { |prev, num| prev << 5 | num }
   end
 
   ## Unpacks a Integer from pack() method into a short string
